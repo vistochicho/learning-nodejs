ed391170ad8521773f282e9e064a31bc4d345b54
vistochicho <vistolution@gmail.com>
2022-10-31 16:30:35 +0700

    debugger

---
+++ debugger.js
@@ -0,0 +1,10 @@
+function sayHello(name) {
+    debugger;
+    return `hello ${name}`;
+}
+
+const name = "Visto";
+debugger;
+console.info(sayHello(name));
+
+// to debug, write node inspect filename.js in terminal
